1b73cb9a637f3c8a4979232d2c52051715848028
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-02-23 14:42:27 +0300

    Add the Database Relation among the different tables

--- models/models.py
+++ models/models.py
@@ -1,10 +1,48 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
-
+from openerp.addons import decimal_precision as dp
 
 class LibraryBook(models.Model):
 	_name='library.book'
+	_description = 'Library of Books'
+	_order = 'date_release desc, name'
+	_rec_name = 'short_name'
 	name = fields.Char('Title', required=True)
-	date_release = fields.Date()
+	short_name = fields.Char(string='Short Title',size=100, translate=False)
+	date_release = fields.Date('Release Date')
 	authors_id = fields.Many2many('res.partner', string="Authors")
+	notes = fields.Text('Internal Notes')
+	state = fields.Selection(
+			[('draft','Not Available'),
+				('available','Available'),
+				('lost','Lost')],'State')
+	cover = fields.Binary('Book Cover')
+	out_of_print = fields.Boolean('Out of Print?')
+	date_update = fields.Datetime('Last Update')
+	pages = fields.Integer(string='Number of Pages',
+			default=0,help="Total book page count",
+			groups='base.group_user',
+			states={'cancel': [('readonly',True)]},
+			copy=True,
+			index=False, 
+			required=False,
+			readonly=False,
+			company_dependent=False,)
+	reader_rating = fields.Float('Reader Average Rating',(14,4))
+	description = fields.Html(string='Description',
+								sanitize=True,
+								strip_style=False,translate=False)
+	cost_price = fields.Float('Book Cost', dp.get_precision('Book Price'))
+	currency_id = fields.Many2one('res.currency', string='Currency')
+	retail_price = fields.Monetary('Retail Price')
+	publisher_id = fields.Many2one(
+		'res.partner',string='Publisher',ondelete='set null',context={}, domain=[],)
+
+
+
+
+
+class ResPartner(models.Model):
+	_inherit='res.partner'
+	books_ids = fields.One2Many('library.book','publisher_id',string='Published Books')
