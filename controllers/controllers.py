a4dbb34f01fba3cc641c1842de7fc79a6e54eeaf
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2017-02-27 11:32:22 +0300

    Adding Path accessible to the network - incomplete

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 from openerp import http
+from openerp.http import request
 
-# class /opt/odoo/danAddons/bookStore(http.Controller):
-#     @http.route('//opt/odoo/dan_addons/book_store//opt/odoo/dan_addons/book_store/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class main(http.Controller):
+	@http.route('/book_store', type='http', auth='none')
+	def books(self):
+		records = request.env['library.book'].sudo().search([])
+		result = '<html><body><table><tr><td>'
+		result += '</td></tr><tr><td>'.join(
+				records.mapped('name')
+			)
+		result += '</td></tr><//table></body></html>'
 
-#     @http.route('//opt/odoo/dan_addons/book_store//opt/odoo/dan_addons/book_store/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('/opt/odoo/dan_addons/book_store.listing', {
-#             'root': '//opt/odoo/dan_addons/book_store//opt/odoo/dan_addons/book_store',
-#             'objects': http.request.env['/opt/odoo/dan_addons/book_store./opt/odoo/dan_addons/book_store'].search([]),
-#         })
+		return result
+		
+	@http.route('/book_store/books/json', type='json', auth='none')
+	def books_json(self):
+		records=request.env['library.book'].sudo().search([])
 
-#     @http.route('//opt/odoo/dan_addons/book_store//opt/odoo/dan_addons/book_store/objects/<model("/opt/odoo/dan_addons/book_store./opt/odoo/dan_addons/book_store"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('/opt/odoo/dan_addons/book_store.object', {
-#             'object': obj
-#         })
+		return records.read(['name'])
+		pass
+
